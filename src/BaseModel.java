d98102e3c0f3656fd608c3937d74f97936def0dc
phuquy2114 <phuquycntt@gmail.com>
2023-02-09 18:55:22 +0700

    init code

---
+++ src/BaseModel.java
@@ -0,0 +1,11 @@
+public class BaseModel <Q>{
+    private Q model;
+
+    public Q getModel() {
+        return model;
+    }
+
+    public void setModel(Q model) {
+        this.model = model;
+    }
+}
---
+++ src/RunMain.java
@@ -0,0 +1,8 @@
+public class RunMain {
+
+    public static void main(String[] args) {
+        BaseModel<Integer> st = new BaseModel<>();
+        st.setModel(10);
+        System.out.println(""+st.getModel());
+    }
+}
